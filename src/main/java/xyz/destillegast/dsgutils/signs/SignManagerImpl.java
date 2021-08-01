fb068667e1cf15e040ce074c89f9ba50118db1ab
DeStilleGast <3677706+DeStilleGast@users.noreply.github.com>
2021-08-01 21:05:43 +0200

    Improved sign update management

--- src/main/java/xyz/destillegast/dsgutils/signs/SignManagerImpl.java
+++ src/main/java/xyz/destillegast/dsgutils/signs/SignManagerImpl.java
@@ -42,7 +42,7 @@ public class SignManagerImpl implements Listener, Runnable, SignManager {
         Bukkit.getScheduler().runTaskTimer(main, this, 20 * 30, 20 * 30);
 
         configFile = new File(main.getDataFolder(), "SignManager.yml");
-        if(!configFile.exists()) {
+        if (!configFile.exists()) {
             try {
                 configFile.createNewFile();
             } catch (IOException e) {
@@ -61,30 +61,32 @@ public class SignManagerImpl implements Listener, Runnable, SignManager {
     }
 
     @Override
-    public void registerHandler(String title, SignActions signActions){
+    public void registerHandler(String title, SignActions signActions) {
         actionHandlers.put(getAsHeader(title), signActions);
         main.logInfo(String.format("%s has been registered by %s", title, signActions.getClass()));
     }
 
     @Override
-    public void unregisterHandler(String title){
+    public void unregisterHandler(String title) {
         actionHandlers.remove(getAsHeader(title));
     }
 
-    public String getAsHeader(String title){
+    public String getAsHeader(String title) {
         return "[" + title + "]";
     }
 
     @EventHandler
-    public void onSignPlace(SignChangeEvent event){
+    public void onSignPlace(SignChangeEvent event) {
         Block block = event.getBlock();
-        if(block.getState() instanceof Sign) {
+        if (block.getState() instanceof Sign) {
             String firstLine = event.getLine(0);
-            if(actionHandlers.containsKey(firstLine)) {
-                boolean allowSign = actionHandlers.get(firstLine).onSignPlace(event.getPlayer(), event.getBlock(), event.getLines());
+            if (actionHandlers.containsKey(firstLine)) {
+                SignActions signAction = actionHandlers.get(firstLine);
+                boolean allowSign = signAction.onSignPlace(event.getPlayer(), event.getBlock(), event.getLines());
 
                 if (allowSign) {
                     signLocations.put(block.getLocation(), firstLine);
+                    signAction.onSignUpdate(event.getPlayer(), event.getBlock());
                 } else {
                     event.getBlock().breakNaturally();
                     event.setCancelled(true);
@@ -94,13 +96,13 @@ public class SignManagerImpl implements Listener, Runnable, SignManager {
     }
 
     @EventHandler
-    public void onSignRemove(BlockBreakEvent event){
+    public void onSignRemove(BlockBreakEvent event) {
         Block block = event.getBlock();
-        if(block.getState() instanceof Sign){
+        if (block.getState() instanceof Sign) {
             String firstLine = ((Sign) block.getState()).getLine(0);
-            if(actionHandlers.containsKey(ChatColor.stripColor(firstLine))) {
+            if (actionHandlers.containsKey(ChatColor.stripColor(firstLine))) {
                 boolean shouldCancel = actionHandlers.get(firstLine).onSignRemove(event.getPlayer(), block);
-                if(!shouldCancel) {
+                if (!shouldCancel) {
                     signLocations.remove(event.getBlock().getLocation());
                     event.setCancelled(true);
                 }
@@ -109,28 +111,35 @@ public class SignManagerImpl implements Listener, Runnable, SignManager {
     }
 
     @EventHandler
-    public void onSignInteract(PlayerInteractEvent event){
-        if(event.hasBlock() && event.getClickedBlock() != null){
+    public void onSignInteract(PlayerInteractEvent event) {
+        if (event.hasBlock() && event.getClickedBlock() != null) {
             Block block = event.getClickedBlock();
-            if(block.getState() instanceof Sign){
+            if (block.getState() instanceof Sign) {
                 String firstLine = ((Sign) block.getState()).getLine(0);
-                if(actionHandlers.containsKey(ChatColor.stripColor(firstLine))) {
+                if (actionHandlers.containsKey(ChatColor.stripColor(firstLine))) {
                     actionHandlers.get(firstLine).onSignInteract(event.getPlayer(), block, event.getAction());
                 }
             }
         }
     }
 
-    private void updateSigns(){
-        actionHandlers.keySet().forEach(this::updateSignByTitle);
+    private void updateSigns() {
+//        actionHandlers.keySet().forEach(this::updateSignByTitle);
+        updateSignByTitle(null);
     }
 
-    private void updateSignByTitle(String title){
+    private void updateSignByTitle(String title) {
         for (Location signLocation : signLocations.keySet()) {
-            if(signLocation.getChunk().isLoaded()){
-                String key = signLocations.get(signLocation);
-                if(key.equals(title)){
-                    Bukkit.getOnlinePlayers().forEach(p -> actionHandlers.get(key).onSignUpdate(p, signLocation.getBlock()));
+            if(signLocation.getWorld() == null) continue;
+
+            String key = signLocations.get(signLocation);
+            if (title != null && key.equals(title) || title == null) {
+                for (Player player : signLocation.getWorld().getPlayers()) {
+                    if (signLocation.distanceSquared(player.getLocation()) <= signLocation.getWorld().getViewDistance() * 16 - 16) {
+                        Bukkit.getScheduler().runTask(main, () -> {
+                            actionHandlers.get(key).onSignUpdate(player, signLocation.getBlock());
+                        });
+                    }
                 }
             }
         }
@@ -139,23 +148,24 @@ public class SignManagerImpl implements Listener, Runnable, SignManager {
 
     @Override
     public void run() {
-        updateSigns();
+        Bukkit.getScheduler().runTaskAsynchronously(main, this::updateSigns);
+//        updateSigns();
     }
 
     @EventHandler
-    public void onPlayerTeleport(PlayerTeleportEvent event){
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
         updateSignsNearLocationForPlayer(event.getPlayer());
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
+    public void onPlayerJoin(PlayerJoinEvent event) {
         updateSignsNearLocationForPlayer(event.getPlayer());
     }
 
-    public void updateSignsNearLocationForPlayer(final Player player){
+    public void updateSignsNearLocationForPlayer(final Player player) {
         Bukkit.getScheduler().runTask(main, () -> {
             for (Location signLocation : signLocations.keySet()) {
-                if(signLocation.getWorld() == player.getWorld()) {
+                if (signLocation.getWorld() == player.getWorld()) {
                     if (signLocation.distance(player.getLocation()) < 64) {
                         String key = signLocations.get(signLocation);
 
@@ -179,13 +189,12 @@ public class SignManagerImpl implements Listener, Runnable, SignManager {
     }
 
 
-
     private String locationToString(Location location) {
         if (location == null) return "";
-        return location.getWorld().getName() + "," + (int)location.getX() + "," + (int)location.getY() + "," + (int)location.getZ();
+        return location.getWorld().getName() + "," + (int) location.getX() + "," + (int) location.getY() + "," + (int) location.getZ();
     }
 
-    private Location stringToLocation(String string){
+    private Location stringToLocation(String string) {
         String[] locParts = string.split(",");
         return new Location(Bukkit.getWorld(locParts[0]), Integer.parseInt(locParts[1]), Integer.parseInt(locParts[2]), Integer.parseInt(locParts[3]));
     }
